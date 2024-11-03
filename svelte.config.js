b977714a9d67edade7334860fdc1e5d3709862f6
Cliff Njoroge <cnjoroge925@gmail.com>
2024-11-03 17:34:14 +0300

    no s

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			envPrefix: "MY_CUSTOM_ORIGIN=https://int-insights.com"
+			envPrefix: "MY_CUSTOM_ORIGIN=http://int-insights.com"
 		})
 	}
 };
