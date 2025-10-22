4f95a8f03a9819c3fb105f9ff9d976c1434d81f6
Cliff Njoroge <cnjoroge925@gmail.com>
2025-10-22 20:47:24 +0300

    test port config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	
 	preview: {
-		host: true,
+		port: 3000,
+		host: true,    // This enables listening on all network interfaces
+	},
+	server: {        // Also add this for development server
+		host: true,    // This enables listening on all network interfaces
 		port: 3000
 	}
 });
