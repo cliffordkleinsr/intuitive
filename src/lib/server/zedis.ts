1e810c89020ded741012bc1218830669722de084
cliffordkleinsr <cnjoroge925@gmail.com>
2026-03-16 12:08:54 +0300

    add zedis uri to.env

--- src/lib/server/zedis.ts
+++ src/lib/server/zedis.ts
@@ -1,3 +1,4 @@
 import { Redis } from 'ioredis';
-
-export const redis = new Redis("redis://default:1Nqsg9ZbmBUgOPLRS5P3@188.245.45.171:6379");
+import { env } from '$env/dynamic/private';
+if (!env.ZEDIS_URI) throw new Error('ZEDIS_URI is not set');
+export const redis = new Redis(env.ZEDIS_URI);
